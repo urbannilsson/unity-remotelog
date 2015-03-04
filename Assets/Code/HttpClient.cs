83c23f4cefeb7acace89d3416d5e513f231bdc76
Urban Nilsson <urban.nilsson@rovio.com>
2015-03-04 16:37:50 +0100

    Added TestScene

---
+++ Assets/Code/HttpClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+public class HttpClient
+{
+	public void PostString(string url, string str)
+	{
+		
+	}
+}
+
---
+++ Assets/Code/RemoteLog.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RemoteLog : MonoBehaviour
+{
+	[SerializeField]
+	private string _remoteHost = "http://192.168.1.111:9292";
+
+	public void Awake()
+	{
+		Application.logMessageReceived += Application_LogMessageReceived;
+	}
+
+	private void Application_LogMessageReceived(string condition, string stackTrace, LogType type)
+	{
+		var url = _remoteHost;
+
+		switch (type)
+		{
+			case LogType.Log:
+				url += "/remotelog/log";
+				break;
+
+			case LogType.Assert:
+			case LogType.Exception:
+			case LogType.Error:
+				url += "/remotelog/error";
+				break;
+
+			case LogType.Warning:
+				url += "/remotelog/warning";
+				break;
+		}
+	}	
+}
